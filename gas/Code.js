b0a5f188319976ccaf1e098ca4c6057701d3931a
Atsushi NAGASE <a@ngs.io>
2020-05-13 08:39:46 +0900

    Create Code.js

---
+++ gas/Code.js
@@ -0,0 +1,29 @@
+function doPost(e) {
+  insert(JSON.parse(e.postData.contents));
+  return ContentService
+    .createTextOutput('{"message": "ok"}')
+    .setMimeType(ContentService.MimeType.JSON);
+}
+
+function testInsert() {
+  insert({
+    name: 'test',
+    subject: 'yo',
+    device: 'aaa',
+    version: '111',
+    message: 'fodsa'
+  });
+}
+
+function insert(data) {
+  var ss = SpreadsheetApp.getActiveSpreadsheet();
+  var sheet = ss.getActiveSheet();
+  sheet.appendRow([
+    new Date(),
+    data.name,
+    data.subject,
+    data.device,
+    data.version,
+    data.message
+  ]);
+}
