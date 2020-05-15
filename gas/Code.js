ce2c59013f3fc6b2cc09736a939ca75e1db3d250
Atsushi Nagase <a@ngs.io>
2020-05-16 07:56:51 +0900

    Add clasp

--- gas/Code.js
+++ gas/Code.js
@@ -1,18 +1,18 @@
 function doPost(e) {
   insert(JSON.parse(e.postData.contents));
-  return ContentService
-    .createTextOutput('{"message": "ok"}')
-    .setMimeType(ContentService.MimeType.JSON);
+  return ContentService.createTextOutput('{"message": "ok"}').setMimeType(
+    ContentService.MimeType.JSON
+  );
 }
 
 function testInsert() {
   insert({
-    name: 'test',
-    subject: 'yo',
-    device: 'aaa',
-    version: '111',
-    message: 'fodsa',
-    email: 'a@ngs.io'
+    name: "test",
+    subject: "yo",
+    device: "aaa",
+    version: "111",
+    message: "fodsa",
+    email: "a@ngs.io"
   });
 }
 
